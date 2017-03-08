78c37cd96e353ba23334af8aad8c1617ee45f699
dhfromkorea <dhllee424@gmail.com>
2017-03-08 15:40:41 +0900

    modified the dependency script to work on OS X
    I acknowledge the commit as it stands is terrible, as it broke the
    dependency script for Linux environments. I committed the crime to
    quickly see the app working.

--- dependencies.sh
+++ dependencies.sh
@@ -9,70 +9,42 @@
 #    opencv >= 2.4.8
 #    django >= 1.6.1
 
-echo "Installing ffmpeg"
-sudo apt-get install build-essential libmp3lame-dev libvorbis-dev libtheora-dev libspeex-dev yasm pkg-config libfaac-dev libopenjpeg-dev libx264-dev libvpx-dev
-mkdir software
-cd software
-wget http://ffmpeg.org/releases/ffmpeg-2.7.2.tar.bz2
-cd ..
-mkdir src
-cd src
-tar xvjf ../software/ffmpeg-2.7.2.tar.bz2
-cd ffmpeg-2.7.2
-./configure --enable-gpl --enable-postproc --enable-swscale --enable-avfilter --enable-libmp3lame --enable-libvorbis --enable-libtheora --enable-libx264 --enable-libspeex --enable-shared --enable-pthreads --enable-libopenjpeg --enable-libfaac --enable-nonfree --enable-libvpx
-sudo make -j 2
-sudo make install
-sudo ldconfig
+# build for os x
+# consult https://trac.ffmpeg.org/wiki/CompilationGuide/MacOSX
+echo "Installing ffmpeg related dependencies"
+brew install ffmpeg lame libvorbis theora speex
+brew install yasm pkg-config faac openjpeg x264 libvpx
 
-echo "Installing python-pip"
-sudo apt-get install python-pip
+mkdir src && cd src
+git clone http://source.ffmpeg.org/git/ffmpeg.git ffmpeg
+cd ffmpeg
 
-echo "Installing OpenCV"
-sudo apt-get install python-opencv
+./configure --enable-gpl --enable-postproc --enable-swscale --enable-avfilter --enable-libmp3lame --enable-libvorbis --enable-libtheora --enable-libx264 --enable-libspeex --enable-shared --enable-pthreads --enable-libvpx --enable-libopenjpeg
 
-echo "Insatlling numpy"
-sudo pip install numpy;
+make -j 2 && make install
 
-echo "Installing scipy"
-sudo pip install scipy;
+# ldconfig
 
-echo "Installing pyaudio"
-#For x86, change amd64 to x86 and it works
-wget https://people.csail.mit.edu/hubert/pyaudio/packages/python-pyaudio_0.2.8-1_amd64.deb;
-sudo dpkg -i python-pyaudio_0.2.8-1_amd64.deb
+echo "Installing other dependencies"
+brew install portaudio opencv nginx
+pip install scipy numpy pyaudio matplotlib pydub gunicorn django MySQL-python
+# ln -s /usr/local/mysql/lib/libmysqlclient.18.dylib /usr/lib/libmysqlclient.18.dylib
 
-echo "Installing matplotlib"
-sudo pip install matplotlib;
-
-echo "Installing pydub"
-sudo pip install pydub;
-
-echo "Installing mysql"
-sudo apt-get install mysql-server
-sudo apt-get install mysql-client
-sudo apt-get install python-dev libmysqlclient-dev
-
-echo "Installing django"
-sudo pip install django;
-
-echo "Installing mysqldb"
-sudo apt-get install python-mysql
+# pip freeze > requirements.txt
+# pip install -r requirements.txt
 
 echo "Installing dejavu"
 git clone https://github.com/vasanthkalingeri/dejavu.git
 cd dejavu
-sudo python setup.py install
+python setup.py install
 
-echo "Installing nginx"
-sudo apt-get install nginx
 
-echo "Installing gunicorn"
-pip install gunicorn
+mysqld
 
 #When port audio has trouble installing run the following
 #Download the .deb package of portaudio, the following is for amd64, change it to x86 if required.
 
 #wget libportaudio-ocaml_0.2.0-1+b2_amd64.deb
-#sudo dpkg -i libportaudio-ocaml_0.2.0-1+b2_amd64.deb
+#dpkg -i libportaudio-ocaml_0.2.0-1+b2_amd64.deb
 ##If dependency errors
-#sudo apt-get -f install 
+#apt-get -f install
--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ CONFIG = {
     "database": {
         "host": "127.0.0.1",
         "user": "root",
-        "passwd": "pass",
+        "passwd": "1234",
         "db": "dejavu"
     }
 }
